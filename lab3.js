d5fd5407ed58206d7bdc82b521ea0e62b8a1a953
ilmarika <ilmari.linna@gmail.com>
2018-02-06 12:12:38 +0200

    js complains that image.is() is not a function

---
+++ lab3.js
@@ -0,0 +1,55 @@
+/* global $ */
+let currentImage;
+function addImage(address) { 
+    // Create a HTML element using jQuery and store it in a variable
+    let element = $('<div>');
+    // ... then you can manipulate element using jQuery
+    element.text('Hello World!');
+    let imageContainer = $('#imageContainer');
+    let image = '<img src='+ address +'>';
+    imageContainer.append(image);
+    console.log(image);
+    if (image.is(':first-child')) {
+        currentImage = image;
+    } else {
+        image.hide();
+    }
+    return image;
+}
+
+function showImage(image) {
+    currentImage.hide();
+    currentImage = image;
+    currentImage.show();
+}
+
+function nextImage() {
+    let image = currentImage.next();
+    if (image.lenght() == 0) {
+        image = imageContainer.children().first();
+    }
+
+    showImage(image);
+}
+
+function prevImage() {
+    let image = currentImage.previous()
+    if (image.lenght() == 0) {
+        image = imageContainer.children().last();
+    }   
+}
+
+function clearImages() {
+    imageContainer.clear();
+}
+addImage("https://placeimg.com/640/480/animals");
+addImage("https://placeimg.com/640/480/animals");
+addImage("https://placeimg.com/640/480/animals");
+
+$('#nextImg').click(function() {
+    nextImage();
+});
+
+$('#prevImg').click(function() {
+    prevImage();
+});

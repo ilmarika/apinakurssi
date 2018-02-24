ea3faf29eb1bb2c2d44d65c24dfc0f28aaaca93e
ilmarika <ilmari.linna@gmail.com>
2018-02-24 18:12:42 +0200

    Message is the value of the message box in the html

--- pb2.js
+++ pb2.js
@@ -1,9 +1,19 @@
+'use strict';
+/* global $ */
 const pb2 = new PB2('https://pb2-2018.jelastic.metropolia.fi/', 'first_app_4578');
-
 const msg = {};
-msg.hello = 'world';
-pb2.sendJson(msg);
+let message = document.getElementById("message");
 
-pb2.setReceiver(function(data) {
-  console.log('socket.on message received: '+JSON.stringify(data));
+function getMessage(){
+  msg.nick = 'ile';
+  msg.msg = message.value;
+  pb2.sendJson(msg);
+
+  pb2.setReceiver(function(data) {
+    console.log('socket.on message received: '+JSON.stringify(data));
+  });
+}
+
+$('#send').click(function() {
+  getMessage();
 });

b9fe0cc2a9a8e2ad5173d8d4fc5a9fa790756d09
ilmarika <ilmari.linna@gmail.com>
2018-02-25 11:20:32 +0200

    set Reveiver function as its own function, prevents the messages to be sent exponentially

--- pb2.js
+++ pb2.js
@@ -9,19 +9,19 @@ function getMessage(){
   msg.nick = nick.value;
   msg.msg = message.value;
   pb2.sendJson(msg);
-
-  pb2.setReceiver(function(data) {
-    console.log('socket.on message received: '+JSON.stringify(data));
-    let div = document.createElement("div");
-    if(data.me) {
-      div.setAttribute("id", "me");
-    } else {
-      div.setAttribute("id", "other");
-    }
-    document.getElementById("messages").appendChild(div);
-    div.innerHTML = data.json.nick + ': ' + data.json.msg;
-  });
 }
+
+pb2.setReceiver(function(data) {
+  console.log('socket.on message received: '+JSON.stringify(data));
+  let div = document.createElement("div");
+  if(data.me) {
+    div.setAttribute("id", "me");
+  } else {
+    div.setAttribute("id", "other");
+  }
+  document.getElementById("messages").appendChild(div);
+  div.innerHTML = data.json.nick + ': ' + data.json.msg;
+});
 
 $('#send').click(function() {
   getMessage();
